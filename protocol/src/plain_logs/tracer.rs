8e4e92f305842a15f6d3fdfef2a54ff86f1a2983
Denis Kolodin <deniskolodin@gmail.com>
2021-06-22 15:32:15 +0300

    Add PlainLogs tracer

--- protocol/src/plain_logs/tracer.rs
+++ protocol/src/plain_logs/tracer.rs
@@ -1,1 +1,17 @@
+use super::flow::PlainLogsState;
+use derive_more::{Deref, DerefMut};
+use rill_engine::tracers::tracer::{Tracer, Watcher};
+use rill_protocol::io::provider::Path;
 
+#[derive(Debug, Deref, DerefMut, Clone)]
+pub struct PlainLogsTracer {
+    tracer: Tracer<PlainLogsState>,
+}
+
+impl PlainLogsTracer {
+    pub fn new(path: Path) -> (Self, Watcher<PlainLogsState>) {
+        let state = PlainLogsState::new();
+        let (tracer, watcher) = Tracer::new_push(state, path);
+        (Self { tracer }, watcher)
+    }
+}
